8b6b510eef3eae3d27f6aac17bb1a22fff03676d
MrTequila <igi444@gmail.com>
2017-10-14 11:20:15 +0200

    Builder Pattern

---
+++ src/com/mrTequila/Builder/TestBuilderPattern.java
@@ -0,0 +1,15 @@
+package com.mrTequila.Builder;
+
+public class TestBuilderPattern {
+
+    public static void main(String[] args) {
+        Computer computer = new Computer.ComputerBuilder("700 GB", "4 GB")
+                .setBluetoothEnabled(true).setGraphicsCardEnabled(true).build();
+
+        Computer computer2 = new Computer.ComputerBuilder("700 GB", "4 GB")
+                .setBluetoothEnabled(true).build();
+
+        System.out.println(computer.toString());
+        System.out.println(computer2.toString());
+    }
+}
