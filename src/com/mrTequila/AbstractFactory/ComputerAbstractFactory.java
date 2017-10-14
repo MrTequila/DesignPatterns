5da62bec5e012e99a1d940d3a3e0d8aebc4f8a25
MrTequila <igi444@gmail.com>
2017-10-14 10:44:18 +0200

    Abstract Factory Pattern

---
+++ src/com/mrTequila/AbstractFactory/ComputerAbstractFactory.java
@@ -0,0 +1,6 @@
+package com.mrTequila.AbstractFactory;
+
+public interface ComputerAbstractFactory {
+
+    public Computer createComputer();
+}
---
+++ src/com/mrTequila/AbstractFactory/ServerFactory.java
@@ -0,0 +1,19 @@
+package com.mrTequila.AbstractFactory;
+
+public class ServerFactory implements ComputerAbstractFactory {
+
+    private String ram;
+    private String hdd;
+    private String cpu;
+
+    public ServerFactory(String ram, String hdd, String cpu) {
+        this.ram = ram;
+        this.cpu = cpu;
+        this.hdd = hdd;
+    }
+    
+    @Override
+    public Computer createComputer() {
+        return new Server(ram, hdd, cpu);
+    }
+}
