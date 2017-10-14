5112cefdc47a6fdb6157cadd5eb701cd544dcabc
MrTequila <igi444@gmail.com>
2017-10-14 20:26:59 +0200

    Decorator Pattern

---
+++ src/com/mrTequila/Structural/Decorator/BasicCar.java
@@ -0,0 +1,9 @@
+package com.mrTequila.Structural.Decorator;
+
+public class BasicCar implements Car {
+
+    @Override
+    public void assemble() {
+        System.out.print("Basic Car.");
+    }
+}
---
+++ src/com/mrTequila/Structural/Decorator/Car.java
@@ -0,0 +1,6 @@
+package com.mrTequila.Structural.Decorator;
+
+public interface Car {
+
+    public void assemble();
+}
